74b834436dbf330c66d77d12062567990ef453f3
tmking2002 <tmking4@ncsu.edu>
2023-05-29 14:02:42 -0400

    update stuff for betting

--- predict_wcws.R
+++ predict_wcws.R
@@ -22,18 +22,18 @@ get_info <- function(matchup){
            home_team_def_rating = defensive_rating)
   
   return(updated)
-
+  
 }
 
 sim_game <- function(matchup_info, num_sims){
   
   away_pred <- predict(model, matchup_info %>% rename(off_rating = away_team_off_rating,
-                                                           opp_def_rating = home_team_def_rating,
-                                                           opp_starter = home_starter))
+                                                      opp_def_rating = home_team_def_rating,
+                                                      opp_starter = home_starter))
   
   home_pred <- predict(model, matchup_info %>% rename(off_rating = home_team_off_rating,
-                                                           opp_def_rating = away_team_def_rating,
-                                                           opp_starter = away_starter))
+                                                      opp_def_rating = away_team_def_rating,
+                                                      opp_starter = away_starter))
   
   scores <- data.frame()
   
@@ -45,19 +45,19 @@ sim_game <- function(matchup_info, num_sims){
     margin <- home_runs - away_runs
     
     while(margin == 0){
-
+      
       away_runs <- rpois(1, away_pred / 7)
       home_runs <- rpois(1, home_pred / 7)
-     
+      
       margin <- home_runs - away_runs
-       
+      
     }
     
     scores <- rbind(scores, data.frame(home_runs, away_runs, margin) %>% 
                       mutate(total = home_runs + away_runs))
-   
+    
     if(i %% 100 == 0) print(i)
-     
+    
   }
   
   return(scores)
@@ -76,24 +76,11 @@ get_stats <- function(scores){
   
 }
 
-#### Oklahoma vs. Clemson ####
 
-matchup <- data.frame(away_team = "Clemson", away_starting_pitcher = "Cagle, Valerie",
-                      home_team = "Oklahoma", home_starting_pitcher = "Storako, Alex")
-
-matchup_info <- get_info(matchup)
-
-scores <- sim_game(matchup_info, 10000)
-
-stats <- get_stats(scores)
-
-stats[[1]]
-stats[[2]]
-
-#### Duke vs. Stanford ####
+#### Oklahoma vs. Stanford ####
 
 matchup <- data.frame(away_team = "Stanford", away_starting_pitcher = "Canady, NiJaree",
-                      home_team = "Duke", home_starting_pitcher = "Curd, Cassidy")
+                      home_team = "Oklahoma", home_starting_pitcher = "Bahl, Jordyn")
 
 matchup_info <- get_info(matchup)
 
@@ -104,39 +91,10 @@ stats <- get_stats(scores)
 stats[[1]]
 stats[[2]]
 
-#### Alabama vs. Northwestern ####
 
-# Fouts
+#### Alabama vs Tennessee ####
 
-matchup <- data.frame(away_team = "Northwestern", away_starting_pitcher = "Williams, Danielle",
-                      home_team = "Alabama", home_starting_pitcher = "Salter, Alex")
-
-matchup_info <- get_info(matchup)
-
-scores <- sim_game(matchup_info, 10000)
-
-stats <- get_stats(scores)
-
-stats[[1]]
-stats[[2]]
-
-# Torrence
-
-matchup <- data.frame(away_team = "Northwestern", away_starting_pitcher = "Williams, Danielle",
-                      home_team = "Alabama", home_starting_pitcher = "Torrence, Jaala")
-
-matchup_info <- get_info(matchup)
-
-scores <- sim_game(matchup_info, 10000)
-
-stats <- get_stats(scores)
-
-stats[[1]]
-stats[[2]]
-
-#### Tennessee vs. Texas ####
-
-matchup <- data.frame(away_team = "Texas", away_starting_pitcher = "Morgan, Mac",
+matchup <- data.frame(away_team = "Alabama", away_starting_pitcher = "Fouts, Montana",
                       home_team = "Tennessee", home_starting_pitcher = "Rogers, Ashley")
 
 matchup_info <- get_info(matchup)
@@ -148,24 +106,10 @@ stats <- get_stats(scores)
 stats[[1]]
 stats[[2]]
 
-#### Florida St. vs. Georgia ####
+#### Florida St. vs Oklahoma St. ####
 
-matchup <- data.frame(home_team = "Florida St.", home_starting_pitcher = "Sandercock, Kathryn",
-                      away_team = "Georgia", away_starting_pitcher = "Walters, Shelby")
-
-matchup_info <- get_info(matchup)
-
-scores <- sim_game(matchup_info, 10000)
-
-stats <- get_stats(scores)
-
-stats[[1]]
-stats[[2]]
-
-#### Oklahoma St. vs. Oregon ####
-
-matchup <- data.frame(away_team = "Oregon", away_starting_pitcher = "Hansen, Stevie",
-                      home_team = "Oklahoma St.", home_starting_pitcher = "Maxwell, Kelly")
+matchup <- data.frame(away_team = "Oklahoma St.", away_starting_pitcher = "Maxwell, Kelly",
+                      home_team = "Florida St.", home_starting_pitcher = "Sandercock, Kathryn")
 
 matchup_info <- get_info(matchup)
 
@@ -176,9 +120,10 @@ stats <- get_stats(scores)
 stats[[1]]
 stats[[2]]
 
-#### Washington vs. Louisiana ####
 
-matchup <- data.frame(away_team = "Louisiana", away_starting_pitcher = "Landry, Sam",
+#### Washington vs. Utah. ####
+
+matchup <- data.frame(away_team = "Utah", away_starting_pitcher = "Lopez, Mariah",
                       home_team = "Washington", home_starting_pitcher = "Meylan, Ruby")
 
 matchup_info <- get_info(matchup)
@@ -189,18 +134,3 @@ stats <- get_stats(scores)
 
 stats[[1]]
 stats[[2]]
-
-#### Utah vs. San Diego St. ####
-
-matchup <- data.frame(away_team = "San Diego St.", away_starting_pitcher = "Light, Allie",
-                      home_team = "Utah", home_starting_pitcher = "Lopez, Mariah")
-
-matchup_info <- get_info(matchup)
-
-scores <- sim_game(matchup_info, 10000)
-
-stats <- get_stats(scores)
-
-stats[[1]]
-stats[[2]]
-
